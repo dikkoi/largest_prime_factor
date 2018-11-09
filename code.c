4e179f6750ddff44b710d78ce00fcde472d1c5e4
Artur Oganesyan <arturgns@gmail.com>
2018-11-09 14:48:55 +0300

    Create code.c

---
+++ code.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+    int func(void);
+int main(void) {
+
+
+    unsigned long num, sqwirt, i, j;
+    printf ("ВВеди число: ");
+    scanf ("%lu", &num);
+    sqwirt = sqrt (num);
+    printf ("squera root of %lu is %lu", num, sqwirt);
+    int k = 0;
+    for (i = sqwirt; i >=2; i--) {
+        j = sqrt (i);
+
+        int g = 0;
+        for (j = sqrt (i); j>=2; j-- ) {
+            if (i%j == 0) {
+                //printf ("\nкорень из корня равен %lu", j);
+                g++;
+                //printf ("\n%d", g);
+                break;
+            }
+        }
+        if (g == 0) {
+            //printf("\n%lu простое проверим как оно делит число num", i);
+            if (num%i == 0) {
+                printf("\n%lu делим число num без остатка", i);
+                break;}
+        }
+
+    }
+    func();
+    return 0;
+}
+int func(void) {
+        printf ("\ncheck\n");
+        return 0;
+    }
